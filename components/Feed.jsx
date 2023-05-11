8e442274bb21dc091eaf10350eb22f03a8e2d804
dzulhilmi <dzulhilmi84@gmail.com>
2023-05-11 10:30:08 +0700

    Add: Feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
